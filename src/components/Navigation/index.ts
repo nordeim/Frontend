0d96dd4b0b43a0673ec81d12b58315b76f31eb5a
heinazhik <heinazhikweymong@outlook.com>
2025-09-15 07:58:14 +0800

    kimi r3.2

---
+++ src/components/Navigation/index.ts
@@ -0,0 +1,17 @@
+// src/components/Navigation/index.ts
+/**
+ * Navigation components export barrel file
+ * @module components/Navigation
+ */
+
+// Mobile navigation
+export { MobileNavigation, FloatingNavButton } from './MobileNavigation';
+export type { MobileNavigationProps, NavigationItem } from './MobileNavigation';
+
+// Bottom navigation
+export { BottomNavigation, BottomNavigationWithFab } from './BottomNavigation';
+export type { BottomNavItem, BottomNavigationProps } from './BottomNavigation';
+
+// Default exports
+export { default as MobileNavigation } from './MobileNavigation';
+export { default as BottomNavigation } from './BottomNavigation';
